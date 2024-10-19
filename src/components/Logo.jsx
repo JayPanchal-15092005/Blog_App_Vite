a7130c583285f81f2bae40f2a1f91d596f41ff63
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2024-10-19 16:07:55 +0530

    logo add

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Logo({width = '100px'}) {
+function Logo({ width = '100px' }) {
   return (
-    <div>Logo</div>
-  )
+    <div>
+      <img src="/logo.png" alt="Logo" style={{ width }} />
+    </div>
+  );
 }
 
 export default Logo
